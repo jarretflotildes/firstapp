ccd85ecfbed00500813f2845df1f193e099a157a
jarret <jflotildes@utexas.edu>
2025-04-05 23:23:35 -0500

    initial commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by miyabi gaming on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
